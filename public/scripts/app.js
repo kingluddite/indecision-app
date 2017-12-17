fbc2af0e70599ebb2757de65354698e3668b15ab
Kingluddite <howley.phil@gmail.com>
2017-12-16 21:42:10 -0800

    add classes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,36 +4,41 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Person = function () {
-  function Person() {
-    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
-    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-    _classCallCheck(this, Person);
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-    this.name = name;
-    this.age = age;
+var Header = function (_React$Component) {
+  _inherits(Header, _React$Component);
+
+  function Header() {
+    _classCallCheck(this, Header);
+
+    return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
   }
 
-  _createClass(Person, [{
-    key: 'getGreeting',
-    value: function getGreeting() {
-      // return 'Hi, I am ' + this.name;
-      return 'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old';
-    }
-  }, {
-    key: 'getDescription',
-    value: function getDescription() {
-      return 'I am ' + this.age + ' years old.';
+  _createClass(Header, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'p',
+        null,
+        'Header Component'
+      );
     }
   }]);
 
-  return Person;
-}();
+  return Header;
+}(React.Component);
 
-var me = new Person('John', 40);
-console.log(me.getGreeting());
-console.log(me.getDescription());
-var other = new Person();
-console.log(other.getGreeting());
-console.log(other.getDescription());
+var jsx = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Title'
+  ),
+  React.createElement(Header, null)
+);
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -1,59 +1,13 @@
-const app = {
-  title: 'Indecision App',
-  subtitle: 'Making React do cool stuff',
-  options: [],
-};
-
-const onFormSubmit = e => {
-  e.preventDefault();
-
-  const option = e.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
+class Header extends React.Component {
+  render() {
+    return <p>Header Component</p>;
   }
+}
 
-  renderApp();
-};
-
-const onBtnClick = e => {
-  if (app.options.length > 0) {
-    app.options = [];
-  }
-
-  renderApp();
-};
-
-const onMakeDecsion = () => {
-  // generate a random number inclusive (0 or 1)
-  // so we can pull item out of array buy it's index
-  const randomNum = Math.floor(Math.random() * app.options.length);
-  const option = app.options[randomNum];
-  console.log(option);
-};
-
-const renderApp = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-      <button disabled={app.options.length === 0} onClick={onMakeDecsion}>
-        What should I do?
-      </button>
-      <button onClick={onBtnClick}>Remove All</button>
-      <ol>{app.options.map(option => <li key={option}>{option}</li>)}</ol>
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="option" />
-        <button>Add Option</button>
-      </form>
-    </div>
-  );
-
-  const appRoot = document.getElementById('app');
-
-  ReactDOM.render(template, appRoot);
-};
-
-renderApp();
+const jsx = (
+  <div>
+    <h1>Title</h1>
+    <Header />
+  </div>
+);
+ReactDOM.render(jsx, document.getElementById('app'));
