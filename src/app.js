11af70f207f23e82cf9845580d6cbe382119c66b
Kingluddite <howley.phil@gmail.com>
2017-12-16 23:01:28 -0800

    add components and nesting

--- src/app.js
+++ src/app.js
@@ -1,13 +1,72 @@
+class IndecsionApp extends React.Component {
+  render() {
+    const title = 'Indecision';
+    const subtitle = 'My computer is my BFF';
+    const options = ['Option Uno', 'Option Dos', 'Option Tres'];
+
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle} />
+        <Action />
+        <Options options={options} />
+        <AddOption />
+      </div>
+    );
+  }
+}
 class Header extends React.Component {
   render() {
-    return <p>Header Component</p>;
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
+      </div>
+    );
   }
 }
 
-const jsx = (
-  <div>
-    <h1>Title</h1>
-    <Header />
-  </div>
-);
-ReactDOM.render(jsx, document.getElementById('app'));
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Options here</p>
+        {this.props.options.map(option => (
+          <Option key={option} optionText={option} />
+        ))}
+        <Option />
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.optionText}</p>
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" value="type something here" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<IndecsionApp />, document.getElementById('app'));
