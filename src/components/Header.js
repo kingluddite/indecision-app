97fa2013bc01c6d5ee7100f4c4f3dd3cc701ead3
Kingluddite <howley.phil@gmail.com>
2017-12-23 02:24:06 -0800

    add foundation for sass

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 const Header = props => (
-  <div>
-    <h1>{props.title}</h1>
-    {props.subtitle && <h2>{props.subtitle}</h2>}
-  </div>
+  <header className="header">
+    <div className="container">
+      <h1 className="header__title">{props.title}</h1>
+      {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+    </div>
+  </header>
 );
 
 Header.defaultProps = {
-  title: 'Indecison',
+  title: 'Indecision',
 };
 
 export default Header;
